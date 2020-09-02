3e938230a7719c4d85478798465586311362c4a4
Clapa Lucian <clapalucian10@gmail.com>
2020-09-02 17:37:16 +0300

    add menu

--- App.js
+++ App.js
@@ -1,18 +1,58 @@
 import React from 'react';
-import {SafeAreaView, StatusBar} from 'react-native';
+import {SafeAreaView, StatusBar, ScrollView} from 'react-native';
 import {PulseAnimation} from './src/loadingpulse/PulseAnimation';
 import {HeartPulseAnimation} from './src/heartpulse/HeartPulse';
-import {NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import styled from 'styled-components/native';
 
 const Stack = createStackNavigator();
+
+const RowContainer = styled.TouchableOpacity`
+  width: 100%;
+  padding-top: 20;
+  padding-bottom: 10;
+  padding-left: 20;
+  padding-right: 20;
+`;
+const Title = styled.Text`
+  font-size: 20;
+`;
+
+const Divider = styled.View`
+  margin-top: 10;
+  width: 100%;
+  height: 1px;
+  background-color: gray;
+`;
+
+const Menu = () => {
+  const navigation = useNavigation();
+
+  const renderRow = (text, onPress) => {
+    return (
+      <RowContainer onPress={onPress}>
+        <Title>{text}</Title>
+        <Divider />
+      </RowContainer>
+    );
+  };
+
+  return (
+    <ScrollView>
+      {renderRow('Heart Pulse', () => navigation.navigate('Heart Pulse'))}
+      {renderRow('Loading Pulse', () => navigation.navigate('Loading Pulse'))}
+    </ScrollView>
+  );
+};
 
 const App = () => {
   return (
     <NavigationContainer>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView />
-      <Stack.Navigator initialRouteName="Heart Pulse">
+      <Stack.Navigator initialRouteName="Menu">
+        <Stack.Screen name="Menu" component={Menu} />
         <Stack.Screen name="Heart Pulse" component={HeartPulseAnimation} />
         <Stack.Screen name="Loading Pulse" component={PulseAnimation} />
       </Stack.Navigator>
